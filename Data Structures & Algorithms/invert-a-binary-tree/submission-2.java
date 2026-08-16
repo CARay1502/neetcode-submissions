2c1e85264631d5f74ff814e09a0a1cafb173605b
Christian Ray <143613191+CARay1502@users.noreply.github.com>
2026-08-15 22:13:32 -0400

    Add: invert-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-2.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
+ ==== recursive solution - Depth First Search ====
+ this solution takes a binary tree and inverts it by inverting the tree recursively in a top-down manner. 
+ 
+    performs much better with runtime than Breadth first search but worse memory performance due to recursion running on the stack.
+
+ step 0: check if tree is empty (if root == null)
+ step 1: swap the nodes left and right pointers
+ step 2: recursively call dfs on the new left child
+ step 3: recursivley call dfs on teh new right child
+ step 4: return the current node (inverted)
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) { return null; }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
+
